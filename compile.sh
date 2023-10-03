5e278adfa59a55ff8023c249e978b565809e3d39
Jack Kennedy <ps24jkennedy@efcts.us>
2023-10-03 13:53:04 -0400

    add --release

--- compile.sh
+++ compile.sh
@@ -1,7 +1,33 @@
+#!/bin/bash
 
+launch=true
+build_type="Debug"
+
+# Loop through the command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --no-launch)
+            launch=false
+            ;;
+        --release)
+            build_type="Release"
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Run the build steps based on the conditions
 mkdir build
-if [ "$1" == "--no-launch" ]; then
-    cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug && make && cd ..
-else
-    cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug && make && ./scoundrills && cd ..
+cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=$build_type && make
+
+# Check the exit status of make
+make_status=$?
+if [ $make_status -eq 0 ] && [ "$launch" = true ]; then
+    ./scoundrills
 fi
+
+cd ..
