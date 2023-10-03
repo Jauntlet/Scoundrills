f7eed29416112a68ab94032b479b9a7fd7ad029c
Jack Kennedy <ps24jkennedy@efcts.us>
2023-10-03 13:35:55 -0400

    if using --no-launch dont launch the game

--- compile.sh
+++ compile.sh
@@ -1,2 +1,7 @@
+
 mkdir build
-cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug && make && ./scoundrills && cd ..
+if [ "$1" == "--no-launch" ]; then
+    cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug && make && cd ..
+else
+    cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug && make && ./scoundrills && cd ..
+fi
