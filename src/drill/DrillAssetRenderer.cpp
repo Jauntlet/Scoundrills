fe812b5abf8c79a546908bd2f730d7da719a6087
Jack Kennedy <ps24jkennedy@efcts.us>
2023-10-04 13:45:13 -0400

    move srand to only be ran once

--- src/drill/DrillAssetRenderer.cpp
+++ src/drill/DrillAssetRenderer.cpp
@@ -14,12 +14,12 @@ void DrillAssetRenderer::init() {
 	_spriteBatch.init();
 
 	_drillAnimation = Jauntlet::Animation(3);
+
+	srand(Jauntlet::Time::getTime());
 }
 
 void DrillAssetRenderer::draw() {
-	srand(static_cast<unsigned int>(Jauntlet::Time::getTime()));
 	_spriteBatch.begin();
-	//_spriteBatch.draw({0,(-40 * 64),(24 * 64),(24 * 64)}, Jauntlet::ResourceManager::getTexture("Textures/Drill Head.png").id);
 	_spriteBatch.draw({ (3 * 64 + (_shake ? (((float)rand()) / RAND_MAX) * SHAKE_AMOUNT - SHAKE_AMOUNT / 2.0f : 0)),(-42 * 64),(18 * 64),(18 * 64) }, _drillAnimation.getUV(), Jauntlet::ResourceManager::getTexture("Textures/Drill Head.png").id);
 	_spriteBatch.end();
 
