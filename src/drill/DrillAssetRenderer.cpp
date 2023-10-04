c9972759a29d91fc8c366efd17417b13bfb1eb87
Jack Kennedy <ps24jkennedy@efcts.us>
2023-10-04 13:17:13 -0400

    fix shaking

--- src/drill/DrillAssetRenderer.cpp
+++ src/drill/DrillAssetRenderer.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <iostream>
 
+
+const float SHAKE_AMOUNT = 10.0f;
+
 DrillAssetRenderer::DrillAssetRenderer() {
 	// Empty
 }
@@ -15,7 +18,7 @@ void DrillAssetRenderer::draw(bool isOn) {
 	srand(static_cast<unsigned int>(Jauntlet::Time::getTime()));
 	_spriteBatch.begin();
 	//_spriteBatch.draw({0,(-40 * 64),(24 * 64),(24 * 64)}, Jauntlet::ResourceManager::getTexture("Textures/Drill Head.png").id);
-	_spriteBatch.draw({ (3 * 64 + (isOn ? (((float)rand()) / RAND_MAX) * 10.0f - 0.5f : 0)),(-42 * 64),(18 * 64),(18 * 64) }, Jauntlet::ResourceManager::getTexture("Textures/Drill Head.png").id);
+	_spriteBatch.draw({ (3 * 64 + (isOn ? (((float)rand()) / RAND_MAX) * SHAKE_AMOUNT - SHAKE_AMOUNT / 2.0f : 0)),(-42 * 64),(18 * 64),(18 * 64) }, Jauntlet::ResourceManager::getTexture("Textures/Drill Head.png").id);
 	_spriteBatch.end();
 	
 	_spriteBatch.render();
