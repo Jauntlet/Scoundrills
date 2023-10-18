a1e66d383a6c868ef52927ac7336de4919e2c40e
Jack Kennedy <ps24jkennedy@efcts.us>
2023-10-18 09:51:19 -0400

    Update generate_appimage.sh

--- generate_appimage.sh
+++ generate_appimage.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Define the game name
+GAMENAME="scoundrills"
+
 # Define the AppDir directory
-APPDIR="Scoundrills.AppDir"
+APPDIR="$GAMENAME.AppDir"
 
 # Clean up any previous build
 rm -rf "$APPDIR"
@@ -10,31 +13,69 @@ rm -rf "$APPDIR"
 mkdir -p "$APPDIR"
 
 # Copy your executable to the AppDir
-cp build/scoundrills "$APPDIR/scoundrills"
+cp build/$GAMENAME "$APPDIR/$GAMENAME"
 
 # Copy the icon to the AppDir
-cp build/Textures/Icon.png "$APPDIR/scoundrills.png"
-
-# Create a desktop file
-cp extras/scoundrills.desktop "$APPDIR/scoundrills.desktop"
+cp build/Textures/Icon.png "$APPDIR/$GAMENAME.png"
 
 # Give the executable execute permission
-chmod +x "$APPDIR/scoundrills"
+chmod +x "$APPDIR/$GAMENAME"
 
 # Copy the contents of the "build" directory recursively
-cp -r build/Fonts "$APPDIR"
-cp -r build/Levels "$APPDIR"
-cp -r build/Shaders "$APPDIR"
-cp -r build/Textures "$APPDIR"
+cp -r build/* "$APPDIR"
 
-# Create aan apprun
-cp extras/AppRun "$APPDIR/AppRun"
+# clean up build files
+rm -r "$APPDIR/cmake_install.cmake"
+rm -r "$APPDIR/CMakeFiles"
+rm -r "$APPDIR/Makefile"
+rm -r "$APPDIR/compile_commands.json"
+rm -r "$APPDIR/CMakeCache.txt"
+
+# Create an apprun
+cat <<EOF > "$APPDIR/AppRun"
+#!/bin/sh
+
+# find what we execute
+SELF=\$(readlink -f "\$0")
+
+# find the directory we execute in
+HERE=\${SELF%/*}
+
+# go to that directory
+cd \${HERE}
+
+# run the game
+./$GAMENAME
+EOF
+
+cat <<EOF > "$APPDIR/README.md"
+Hi! This is the readme file for your generated appimage. You'll only really see this if you've extracted it to figure out what exactly we do and why we do it. appimages are terribly documented (from what we could find) and dont seem to really mention any standards. The jaunlet team apologizes for any issues caused by this, and asks that if you have any improvements to our appimage generation that you make a commit fixing our script.<br>
+https://github.com/XanderMooney/Scoundrills/tree/Jauntlet-Template
+EOF
 
 # Give the executable execute permission
 chmod +x "$APPDIR/AppRun"
+
+
+wget -nc https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+
+chmod +x appimagetool-x86_64.AppImage
+
+# Create a desktop file
+cat <<EOF > "$APPDIR/$GAMENAME.desktop"
+[Desktop Entry]
+Name=Scoundrills
+Comment=Drill through the earth with convicts
+Exec=scoundrills
+Icon=scoundrills
+Type=Application
+Categories=Game;
+EOF
+
+bash compile.sh --release --no-launch
 
 # Create the AppImage
 ./appimagetool-x86_64.AppImage "$APPDIR"
 
 # Clean up newest build
-rm -rf "$APPDIR"
+#rm -rf "$APPDIR"
