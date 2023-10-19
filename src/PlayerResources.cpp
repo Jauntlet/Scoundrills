f31d0e2178498e709aca7e27175324d174540d80
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-10-19 12:27:02 -0400

    simplify playerResources

--- src/PlayerResources.cpp
+++ src/PlayerResources.cpp
@@ -1,93 +1,10 @@
 #include "PlayerResources.h"
 
-PlayerResources::PlayerResources() : _water(0), _food(0), _copper(0), _parts(0), _heat(0) {
+PlayerResources::PlayerResources() {
 	//empty
 }
 
-PlayerResources::PlayerResources(unsigned int water, unsigned int food, unsigned int copper, unsigned int parts, unsigned int heat) : _water(water), _food(food), _copper(copper), _parts(parts), _heat(heat) {
+PlayerResources::PlayerResources(float Water, unsigned int Food, unsigned int Copper, unsigned int Parts, float Heat) 
+	: water(Water), food(Food), copper(Copper), parts(Parts), heat(Heat) {
 	//empty
-}
-
-//getters
-unsigned int PlayerResources::getWater() {
-	return _water;
-}
-
-unsigned int PlayerResources::getFood() {
-	return _food;
-}
-
-unsigned int PlayerResources::getCopper() {
-	return _copper;
-}
-
-unsigned int PlayerResources::getParts() {
-	return _parts;
-}
-
-unsigned int PlayerResources::getHeat() {
-	return _heat;
-}
-
-// 'w' = Water, 'f' = Food, 'c' = Copper, 'p' = Parts, 'h' = Heat
-unsigned int PlayerResources::get(char code) {
-	switch (code)
-	{
-	case 'w':
-		return _water;
-	case 'f':
-		return _food;
-	case 'c':
-		return _copper;
-	case 'p':
-		return _parts;
-	case 'h':
-		return _heat;
-	default:
-		return 0;
-	}
-}
-
-void PlayerResources::setWater(unsigned int value) {
-	_water = value;
-}
-
-void PlayerResources::setFood(unsigned int value) {
-	_food = value;
-}
-
-void PlayerResources::setCopper(unsigned int value) {
-	_copper = value;
-}
-
-void PlayerResources::setParts(unsigned int value) {
-	_parts = value;
-}
-
-void PlayerResources::setHeat(unsigned int value) {
-	_heat = value;
-}
-
-// 'w' = Water, 'f' = Food, 'c' = Copper, 'p' = Parts, 'h' = Heat
-void PlayerResources::set(char code, unsigned int value) {
-	switch (code)
-	{
-	case 'w':
-		_water = value;
-		break;
-	case 'f':
-		_food = value;
-		break;
-	case 'c':
-		_copper = value;
-		break;
-	case 'p':
-		_parts = value;
-		break;
-	case 'h':
-		_heat = value;
-		break;
-	default:
-		break;
-	}
 }
--- src/PlayerResources.h
+++ src/PlayerResources.h
@@ -6,32 +6,9 @@
 class PlayerResources {
 public:
 	PlayerResources();
-	PlayerResources(unsigned int water, unsigned int food, unsigned int copper, unsigned int parts, unsigned int heat); //with starting resources
+	PlayerResources(float Water, unsigned int Food, unsigned int Copper, unsigned int Parts, float Heat); //with starting resources
 
-	//getters
-	unsigned int getWater();
-	unsigned int getFood();
-	unsigned int getCopper();
-	unsigned int getParts();
-	unsigned int getHeat();
-
-	// 'w' = Water, 'f' = Food, 'c' = Copper, 'p' = Parts, 'h' = Heat
-	unsigned int get(char code); //custom getter
-
-	//setters
-	void setWater(unsigned int value);
-	void setFood(unsigned int value);
-	void setCopper(unsigned int value);
-	void setParts(unsigned int value);
-	void setHeat(unsigned int value);
-
-	// 'w' = Water, 'f' = Food, 'c' = Copper, 'p' = Parts, 'h' = Heat
-	void set(char code, unsigned int value);//custom setter
-
+	unsigned int food = 0, copper = 0, parts = 0;
+	float water = 0.0f, heat = 0.0f;
 private:
-	unsigned int _water;
-	unsigned int _food;
-	unsigned int _copper;
-	unsigned int _parts;
-	unsigned int _heat;
 };
