ab54d06c2936bf5413aea1150b6fab366702060f
Xander Mooney <ps24XMooney@efcts.us>
2023-10-19 18:44:07 -0400

    Compact PathRenderer code
    The sins we commit for compiler efficiency grows larger every day

--- src/pathfinding/PathRenderer.cpp
+++ src/pathfinding/PathRenderer.cpp
@@ -22,79 +22,57 @@ void PathRenderer::createPath(glm::vec2 start, glm::vec2 end) {
 	path.pop_back();
 
 	_spriteBatch.begin();
-	glm::ivec2 direction = glm::vec2(0), lastDir  = glm::vec2(0);
+	glm::ivec2 direction = glm::ivec2(0), lastDir  = glm::ivec2(0);
  	while (!path.empty()) {
 		if (path.size() > 1) {
 			direction = glm::sign(path[0] - path.back());
 
 			if (direction != lastDir && lastDir != glm::ivec2(0)) {
-				if (lastDir.y == 1 && direction.x == -1 || direction.y == -1 && lastDir.x == 1) { // from going down to going right and vice versa
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 6.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				if (lastDir.x == 1) {
+					if (direction.y == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 8.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+					else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 6.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 				}
-				else if (lastDir.y == 1 && direction.x == 1 || direction.y == -1 && lastDir.x == -1) { // from going down to going left and vice versa
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 7.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				else if (lastDir.x == -1) {
+					if (direction.y == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 9.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+					else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 7.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 				}
-				else if (lastDir.y == -1 && direction.x == -1 || direction.y == 1 && lastDir.x == 1) { // from going up to going right and vice versa
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 8.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				else if (lastDir.y == 1) {
+					if (direction.x == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 7.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+					else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 6.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 				}
-				else if (lastDir.y == -1 && direction.x == 1 || direction.y == 1 && lastDir.x == -1) { // from going up to going left and vice versa
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 9.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				else if (lastDir.y == -1) {
+					if (direction.x == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 9.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+					else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 8.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 				}
 			}
-			else if (direction.x == 1) {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 4.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-			}
-			else if (direction.x == -1) {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 4.0f, 0, (1.0f / 18.0f), 1 }, M_PI /*180 degrees in radians*/, _textureID);
-			}
-			else if (direction.y == 1) {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 5.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-			}
-			else {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 5.0f, 0, (1.0f / 18.0f), 1 }, M_PI /*180 degrees in radians*/, _textureID);
-			}
+			else if (direction.x == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 4.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+			else if (direction.x == -1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 4.0f, 0, (1.0f / 18.0f), 1 }, M_PI /*180 degrees in radians*/, _textureID);
+			else if (direction.y == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 5.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+			else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 5.0f, 0, (1.0f / 18.0f), 1 }, M_PI /*180 degrees in radians*/, _textureID);
 		}
 		else { // we are at the part of the line, we draw the arrow head.
 			direction = glm::sign(path[0] - end);
 
 			if (direction != lastDir) {
-				if (direction.x == -1 && lastDir.y == 1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 10.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				if (direction.x == 1) {
+					if (lastDir.y == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 11.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+					else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 17.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 				}
-				else if (direction.x == 1 && lastDir.y == 1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 11.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				else if (direction.x == -1) {
+					if (lastDir.y == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 10.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+					else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 16.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 				}
-				else if (direction.y == -1 && lastDir.x == 1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 12.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				else if (direction.y == 1) {
+					if (lastDir.x == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 14.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+					else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 15.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 				}
-				else if (direction.y == -1 && lastDir.x == -1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 13.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-				}
-				else if (direction.y == 1 && lastDir.x == 1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 14.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-				}
-				else if (direction.y == 1 && lastDir.x == -1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 15.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-				}
-				else if (direction.x == -1 && lastDir.y == -1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 16.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-				}
-				else if (direction.x == 1 && lastDir.y == -1) {
-					_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 17.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-				}
+				else if (lastDir.x == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 12.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+				else _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 13.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 			}
-			else if (direction.x == -1) {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { 0, 0, (1.0f / 18.0f), 1 }, _textureID);
-			}
-			else if (direction.x == 1) {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 3.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-			}
-			else if (direction.y == -1) {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f), 0, (1.0f / 18.0f), 1 }, _textureID);
-			}
-			else if (direction.y == 1) {
-				_spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 2.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
-			}
+			else if (direction.x == -1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { 0, 0, (1.0f / 18.0f), 1 }, _textureID);
+			else if (direction.x == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 3.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
+			else if (direction.y == -1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f), 0, (1.0f / 18.0f), 1 }, _textureID);
+			else if (direction.y == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 2.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 		}
 
 		lastDir = direction;
