63e2ab1be468b1a8c9c9e37afccdab510ce7f8b2
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-11-10 09:22:49 -0500

    fix #6

--- src/pathfinding/PathRenderer.cpp
+++ src/pathfinding/PathRenderer.cpp
@@ -2,6 +2,7 @@
 #include <Jauntlet/JMath.h>
 #include "../players/PlayerManager.h"
 
+#include <iostream>
 PathRenderer::PathRenderer(Jauntlet::TileMap* tileMap, PlayerManager* players) : 
 	_textureID(Jauntlet::ResourceManager::getTexture("Textures/Arrow Paths.png").id), 
 	_players(players),
@@ -18,11 +19,13 @@ void PathRenderer::createPath(glm::vec2 start, glm::vec2 end) {
 
 	glm::vec2 lastPos = start;
 	
+	path.pop_back();
+	//path.erase(path.begin());
 	path[0] = path.back();
 	path.pop_back();
 
 	_spriteBatch.begin();
-	glm::ivec2 direction = glm::ivec2(0), lastDir  = glm::ivec2(0);
+	glm::ivec2 direction = glm::ivec2(0), lastDir  = glm::sign(start - path[0]);
  	while (!path.empty()) {
 		if (path.size() > 1) {
 			direction = glm::sign(path[0] - path.back());
@@ -74,7 +77,6 @@ void PathRenderer::createPath(glm::vec2 start, glm::vec2 end) {
 			else if (direction.y == -1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f), 0, (1.0f / 18.0f), 1 }, _textureID);
 			else if (direction.y == 1) _spriteBatch.draw({ path[0].x, path[0].y, 64, 64 }, { (1.0f / 18.0f) * 2.0f, 0, (1.0f / 18.0f), 1 }, _textureID);
 		}
-
 		lastDir = direction;
 		path[0] = path.back(); 
 		path.pop_back();
