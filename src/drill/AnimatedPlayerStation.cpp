83b60da65a01a787a056f981febd0e193c7a9698
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-11-21 12:14:16 -0500

    AnimatedPlayerStations

---
+++ src/drill/AnimatedPlayerStation.cpp
@@ -0,0 +1,11 @@
+#include "AnimatedPlayerStation.h"
+
+AnimatedPlayerStation::AnimatedPlayerStation(std::string texture, glm::vec4 destination, unsigned int frames, glm::vec4 boundingBox, glm::vec2 anchorPointOffset)
+: PlayerStation(texture, destination, boundingBox, anchorPointOffset), animation(frames) {
+
+}
+
+void AnimatedPlayerStation::draw(Jauntlet::SpriteBatch& spriteBatch) {
+	animation.update();
+	spriteBatch.draw(_destination, animation.getUV(), _textureID);
+}
