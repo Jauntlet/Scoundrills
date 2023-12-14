cc8872ade2faf2ab4b1a9e71d0448f62ced86ee9
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-12-14 14:11:29 -0500

    Pause menu progress

--- src/scenes/PauseMenu.cpp
+++ src/scenes/PauseMenu.cpp
@@ -1,12 +1,20 @@
 #include "PauseMenu.h"
 PauseMenu::PauseMenu(Jauntlet::TextureCache& textureCache) :
 	_resumeButton(&GlobalContext::inputManager, std::bind(&PauseMenu::switchState, this, PauseState::HIDDEN), textureCache.getTexture("Textures/UIbutton.png").id, &_resumeButtonPos, glm::vec2(600, 200), Jauntlet::UIElement::ORIGIN_PIN::CENTER),
-	_settingsButton(&GlobalContext::inputManager, std::bind(&PauseMenu::hideAll, this), textureCache.getTexture("Textures/UIbutton.png").id, &_settingsButtonPos, glm::vec2(600, 200), Jauntlet::UIElement::ORIGIN_PIN::CENTER)
+	_settingsButton(&GlobalContext::inputManager, std::bind(&PauseMenu::switchState, this, PauseState::SETTINGS), textureCache.getTexture("Textures/UIbutton.png").id, &_settingsButtonPos, glm::vec2(600, 200), Jauntlet::UIElement::ORIGIN_PIN::CENTER),
+	_quitButton(&GlobalContext::inputManager, std::bind(&PauseMenu::switchState, this, PauseState::HIDDEN), textureCache.getTexture("Textures/UIbutton.png").id, &_quitButtonPos, glm::vec2(600, 200), Jauntlet::UIElement::ORIGIN_PIN::CENTER),
+	_resumeTextElement(GlobalContext::textRenderer, &_resumeText, &_textColor, &_resumeButtonPos, Jauntlet::UIElement::ORIGIN_PIN::CENTER, 0.5f),
+	_settingsTextElement(GlobalContext::textRenderer, &_settingsText, &_textColor, &_settingsButtonPos, Jauntlet::UIElement::ORIGIN_PIN::CENTER, 0.5f),
+	_quitTextElement(GlobalContext::textRenderer, &_quitText, &_textColor, &_quitButtonPos, Jauntlet::UIElement::ORIGIN_PIN::CENTER, 0.5f)
 {
 	_uiManager.setScale(GlobalContext::screenSize.y / 1080.0f);
 
 	_uiManager.addElement(&_resumeButton, &GlobalContext::normalShader);
+	_uiManager.addElement(&_resumeTextElement, &Jauntlet::TextRenderer::textShader);
 	_uiManager.addElement(&_settingsButton, &GlobalContext::normalShader);
+	_uiManager.addElement(&_settingsTextElement, &Jauntlet::TextRenderer::textShader);
+	_uiManager.addElement(&_quitButton, &GlobalContext::normalShader);
+	_uiManager.addElement(&_quitTextElement, &Jauntlet::TextRenderer::textShader);
 	
 	_uiManager.optimize();
 	_uiManager.resolvePositions();
@@ -30,7 +38,11 @@ void PauseMenu::togglePauseMenu() {
 
 void PauseMenu::hideAll() {
 	_resumeButton.visible = false;
+	_resumeTextElement.visible = false;
 	_settingsButton.visible = false;
+	_settingsTextElement.visible = false;
+	_quitButton.visible = false;
+	_quitTextElement.visible = false;
 }
 
 void PauseMenu::windowResized() {
@@ -50,6 +62,10 @@ void PauseMenu::switchState(PauseState state) {
 
 	if (_state == PauseState::PAUSED) {
 		_resumeButton.visible = true;
+		_resumeTextElement.visible = true;
 		_settingsButton.visible = true;
+		_settingsTextElement.visible = true;
+		_quitButton.visible = true;
+		_quitTextElement.visible = true;
 	}
 }
