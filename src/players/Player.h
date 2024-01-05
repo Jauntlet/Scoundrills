f070a8062d26375b3a4910b7ce0471f844a2f671
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2024-01-05 12:46:03 -0500

    create player with predetermined health

--- src/players/Player.h
+++ src/players/Player.h
@@ -15,6 +15,7 @@ class Player
 {
 public:
 	Player(const glm::vec2& position, uint8_t playerID, bool isCop = false);
+	Player(const glm::vec2& position, uint8_t playerID, int health, bool isCop = false);
 	~Player();
 
 	void update(DrillManager& drill);
