d53c8683749961c5332b452e33572b6fe92d7e19
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2024-01-10 14:03:37 -0500

    Officer walking code

--- src/scenes/Tutorial/Officer.cpp
+++ src/scenes/Tutorial/Officer.cpp
@@ -1,4 +1,5 @@
 #include "Officer.h"
+#include <Jauntlet/Time.h>
 
 Officer::Officer(const glm::vec2& position) :
 	_position(position)
@@ -7,9 +8,21 @@ Officer::Officer(const glm::vec2& position) :
 	_animation.play(0, 1, 0.5f);
 }
 
-void Officer::draw() {
+void Officer::draw(const Jauntlet::Camera2D& camera) {
+	if (_walking) {
+		_position += glm::vec2(300.0f * Jauntlet::Time::getDeltaTime());
+	}
+
 	_animation.update();
 	_batch.begin();
 	_batch.draw(glm::vec4(_position, 64, 64), _animation.getUV(), _textureID);
 	_batch.endAndRender();
+}
+
+void Officer::walkOffscreen() {
+	_animation.play(2, 7, 0.05f);
+	_walking = true;
+}
+bool Officer::isWalking() {
+	return _walking;
 }
